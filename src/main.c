1954fc442182634be9098b12bde56b11898b61a9
HeyShafty <nathan.lecorchet@epitech.eu>
2020-05-05 12:59:17 +0200

    Oui.sh

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2018
+** main.c
+** File description:
+** Main function
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+    argv[argc * 0] = "";
+    return (0);
+}
