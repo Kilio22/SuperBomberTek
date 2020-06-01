48833c7b6e33e83031eb22fce570aa7dff35e25b
Thomas Bastien <thomas1.bastien@epitech.eu>
2020-06-01 11:48:39 +0200

    Revert timer

--- src/systems/TimerStopSystem.cpp
+++ src/systems/TimerStopSystem.cpp
@@ -7,34 +7,15 @@
 
 #include "TimerStopSystem.hpp"
 #include "TimerComponent.hpp"
-#include "PositionComponent.hpp"
-#include "BombComponent.hpp"
-#include "MapComponent.hpp"
 
 using namespace Indie::Components;
 
 void Indie::Systems::TimerStopSystem::onUpdate(irr::f32 deltaTime, EntityManager &entityManager) const
 {
-    MapComponent *mapComponent;
-    std::vector<std::vector<Indie::Components::OBJECT>> map;
-
-    for (auto entity : entityManager.each<MapComponent>()) {
-        mapComponent = entity->getComponent<MapComponent>();
-        map = mapComponent->getMap();
-    }
-
-    for (auto entity : entityManager.each<TimerComponent, PositionComponent>()) {
+    for (auto entity : entityManager.each<TimerComponent>()) {
         auto timer = entity->getComponent<TimerComponent>();
-        auto position = entity->getComponent<PositionComponent>();
 
-        int posX = (int)position->getPosition().X / 20;
-        int posZ = (int)position->getPosition().Z / 20;
-
-        if (timer->getTimePassed() >= timer->getTimeToEnd()) {
-            if (map[posZ][posX] == OBJECT::LAVA && !entity->has<BombComponent>())
-                map[posZ][posX] = OBJECT::VOID;
+        if (timer->getTimePassed() >= timer->getTimeToEnd())
             entity->needDestroy();
-        }
     }
-    mapComponent->setMap(map);
 }
