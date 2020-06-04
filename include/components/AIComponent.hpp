bfd77659089fd669188e0dfdf3a4a88ca8a8c79e
Thomas Bastien <thomas1.bastien@epitech.eu>
2020-06-04 12:53:48 +0200

    Feat: add briefs for documentation

--- include/components/AIComponent.hpp
+++ include/components/AIComponent.hpp
@@ -29,14 +29,10 @@ namespace Indie::Components
     enum class ACTION
     {
         GO_BOX,
-        GO_PLAYER,
         GO_SAFE,
-        GO_POWERUP,
         FIND_BOX,
-        FIND_POWERUP,
         PLACE_BOMB,
         BOMB_PLACED,
-        WAIT_EXPLOSION,
         DODGE,
         STANDBY
     };
@@ -50,19 +46,60 @@ namespace Indie::Components
         AIComponent() = default;
         ~AIComponent() = default;
 
+        /**
+         * @brief Checks if the ai has moved to the next position (in center of the case)
+         * @return true Ai has move
+         * @return false Ai hasn't move yet
+         */
         bool hasMoved(irr::core::vector3df position, irr::core::vector3df nextPosition, AIComponent *ai) const;
 
-        void forceNextDirection(irr::core::vector2di nextPos);
+        /**
+         * @brief Sets the nextDirection
+         * @param map The pathfinding map with the shortless path
+         * @param acPos Actual position of AI
+         */
         void setNextDirection(std::vector<std::vector<OBJECT>> &map, irr::core::vector2di acPos);
+
+        /**
+         * @brief Sets the direction of AI
+         * @param direction Enum Direction: All direction actions for the AI
+         */
         void setDirection(DIRECTION direction);
+
+        /**
+         * @brief Sets the action of AI
+         * @param action Enum Action: All possible actions for the AI
+         */
         void setAction(ACTION action);
 
+        /**
+         * @brief Sets behavior of AI, the behvior depends ont the number of enemies in the same area with the AI
+         * @param value The behavior value
+         */
         void setBehavior(unsigned int value);
+
+        /**
+         * @brief Gets the behavior value of AI
+         * @return The behavior value
+         */
         unsigned int getBehavior() const;
 
+        /**
+         * @brief Gets the actual direction of the AI
+         * @return The direction value
+         */
         DIRECTION getDirection() const;
+
+        /**
+         * @brief Gets the actual action of AI
+         * @return The action value
+         */
         ACTION getAction() const;
 
+        /**
+         * @brief Gets the next postion of AI
+         * @return The vector of the nextPosition
+         */
         irr::core::vector2di getNextPosition() const;
 
     private:
--- include/components/PathFinderComponent.hpp
+++ include/components/PathFinderComponent.hpp
@@ -25,13 +25,40 @@ namespace Indie::Components
         PathFinderComponent() = default;
         ~PathFinderComponent() = default;
 
+        /**
+         * @brief Sets the path map of AI
+         * @param map The path map
+         */
         void setMap(std::vector<std::vector<OBJECT>> map);
+
+        /**
+         * @brief Sets the map with current explosions and future explosions with the range for each bomb
+         * @param map The bombed map
+         */
         void setMapBomb(std::vector<std::vector<OBJECT>> map);
+
+        /**
+         * @brief Sets the final position for the movement of AI
+         * @param endPos The final position vector
+         */
         void setEndMapPos(irr::core::vector2di endPos);
 
+        /**
+         * @brief Gets the path map
+         * @return The path map
+         */
         std::vector<std::vector<OBJECT>> getMap() const;
 
+        /**
+         * @brief Gets the bombed map
+         * @return The bombed map
+         */
         std::vector<std::vector<OBJECT>> getMapBomb() const;
+
+        /**
+         * @brief Gets the final position of AI
+         * @return The final position vector
+         */
         irr::core::vector2di getEndMapPos() const;
 
     private:
