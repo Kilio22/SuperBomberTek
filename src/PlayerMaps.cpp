dd2d72bb4128ec8797e4ee82652adb30837abe99
HeyShafty <nathan.lecorchet@epitech.eu>
2020-06-09 12:52:32 +0200

    Update required levels for maps & skins

--- src/PlayerMaps.cpp
+++ src/PlayerMaps.cpp
@@ -10,9 +10,9 @@
 const std::vector<Indie::PlayerMaps::PlayerMap> Indie::PlayerMaps::mapPaths = {
     { "Defaut", 0U },
     { "Procedurale", 0U },
+    { "../ressources/maps/Empty.supermap", 0U },
     { "../ressources/maps/Lagon.supermap", 2U },
-    { "../ressources/maps/Esport.supermap", 4U },
-    { "../ressources/maps/Maze.supermap", 6U },
-    { "../ressources/maps/Suico.supermap", 8U },
-    { "../ressources/maps/Empty.supermap", 10U },
+    { "../ressources/maps/Maze.supermap", 14U },
+    { "../ressources/maps/Suico.supermap", 16U },
+    { "../ressources/maps/Esport.supermap", 18U },
 };
--- src/PlayerSkins.cpp
+++ src/PlayerSkins.cpp
@@ -17,7 +17,7 @@ const std::vector<Indie::PlayerSkins::PlayerSkin> Indie::PlayerSkins::skinPaths 
     { "../ressources/textures/character/Gris+.png", Components::PlayerComponent::PLAYER_COLOR::GENERIC, 7U },
     { "../ressources/textures/character/Bleu+.png", Components::PlayerComponent::PLAYER_COLOR::BLUE, 9U },
     { "../ressources/textures/character/Rouge+.png", Components::PlayerComponent::PLAYER_COLOR::RED, 11U },
-    { "../ressources/textures/character/Vert+.png", Components::PlayerComponent::PLAYER_COLOR::GREEN, 12U },
-    { "../ressources/textures/character/Violet+.png", Components::PlayerComponent::PLAYER_COLOR::PURPLE, 13U },
-    { "../ressources/textures/character/Jaune+.png", Components::PlayerComponent::PLAYER_COLOR::YELLOW, 14U },
+    { "../ressources/textures/character/Vert+.png", Components::PlayerComponent::PLAYER_COLOR::GREEN, 13U },
+    { "../ressources/textures/character/Violet+.png", Components::PlayerComponent::PLAYER_COLOR::PURPLE, 15U },
+    { "../ressources/textures/character/Jaune+.png", Components::PlayerComponent::PLAYER_COLOR::YELLOW, 17U },
 };
