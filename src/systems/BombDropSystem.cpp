107857541df3db6c4273cd7a96dae597c00968dc
HeyShafty <nathan.lecorchet@epitech.eu>
2020-06-10 17:55:16 +0200

    Reworked GUI in GameScene.

--- src/systems/BombDropSystem.cpp
+++ src/systems/BombDropSystem.cpp
@@ -15,7 +15,7 @@ using namespace Indie::Components;
 std::vector<std::vector<Indie::Components::OBJECT>> Indie::Systems::BombDropSystem::updateMap(
     std::vector<std::vector<Components::OBJECT>> currentMap, irr::f32 midX, irr::f32 midZ) const
 {
-    currentMap[(int)(midZ / 20)][(int)(midX / 20)] = OBJECT::BOMB;
+    currentMap.at((int)(midZ / 20)).at((int)(midX / 20)) = OBJECT::BOMB;
     return currentMap;
 }
 
