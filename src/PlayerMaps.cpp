e19cd7dd973017d6d49fa7ef5f3d33ea4c19ed3e
Thomas Bastien <thomas1.bastien@epitech.eu>
2020-06-10 19:09:42 +0200

    Feat: rename map with french name and add new map to unlockable map array

--- src/PlayerMaps.cpp
+++ src/PlayerMaps.cpp
@@ -10,11 +10,14 @@
 const std::vector<Indie::PlayerMaps::PlayerMap> Indie::PlayerMaps::mapPaths = {
     { "Defaut", 0U },
     { "Procedurale", 0U },
-    { "../ressources/maps/Empty.supermap", 0U },
+    { "../ressources/maps/Vide.supermap", 0U },
     { "../ressources/maps/Lagon.supermap", 2U },
     { "../ressources/maps/Tactique.supermap", 4U },
     { "../ressources/maps/Jardin_Glissant.supermap", 6U },
-    { "../ressources/maps/Maze.supermap", 14U },
+    { "../ressources/maps/Fontaine.supermap", 8U },
+    { "../ressources/maps/Montagne.supermap", 10U },
+    { "../ressources/maps/Labyrinthe.supermap", 12U },
+    { "../ressources/maps/Ferme.supermap", 14U },
     { "../ressources/maps/Suico.supermap", 16U },
     { "../ressources/maps/Esport.supermap", 18U },
 };
