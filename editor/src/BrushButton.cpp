6e5308c90c61363deddaad9ce6b59a75e4b03311
guilian.ganster@epitech.eu <guilian.ganster@epitech.eu@epitech.eu>
2020-06-13 01:22:36 +0200

    button state

--- editor/src/BrushButton.cpp
+++ editor/src/BrushButton.cpp
@@ -8,7 +8,20 @@
 #include "BrushButton.hpp"
 
 BrushButton::BrushButton(EDITOR::TILETYPE type) {
+    _selectedState = false;
+    _pressedState = false;
 
+    switch (type)
+    {
+        case EDITOR::TILETYPE::EMPTY:
+
+            break;
+        case EDITOR::TILETYPE::DESTRUCTIBLE:
+            break;
+        case EDITOR::TILETYPE::UNDESTRUCTIBLE:
+            break;
+        default: break;
+    }
 }
 
 void BrushButton::draw(sf::RenderTarget &target, sf::RenderStates states) const {
@@ -16,5 +29,13 @@ void BrushButton::draw(sf::RenderTarget &target, sf::RenderStates states) const 
 }
 
 void BrushButton::update(sf::RenderWindow &window) {
+
+}
+
+bool BrushButton::isButtonHover(int mx, int my) const {
+
+}
+
+bool BrushButton::isButtonClicked(int mx, int my) const {
     
 }
